6d3f66812969459fc6ca4b4c2f0b2a75d4c7f94c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-08-14 12:47:57 +0200

    implement Point#distance(Point)

--- src/main/java/org/comroid/math/model/Point.java
+++ src/main/java/org/comroid/math/model/Point.java
@@ -2,6 +2,9 @@ package org.comroid.math.model;
 
 import org.comroid.api.Named;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public interface Point extends MathOP<Vector, Point>, Named, DimensionalAnchor {
     float getX();
 
@@ -14,5 +17,15 @@ public interface Point extends MathOP<Vector, Point>, Named, DimensionalAnchor {
     @Override
     default String getAlternateName() {
         return String.format("%s<%f;%f;%f>", getClass().getSimpleName(), getX(), getY(), getZ());
+    }
+
+    default float distance(Point to) {
+        switch (getDimension()) {
+            case Dim2:
+                return (float) sqrt(pow(to.getX() - getX(), 2) + pow(to.getY() - getY(), 2));
+            case Dim3:
+                return (float) sqrt(pow(to.getX() - getX(), 2) + pow(to.getY() - getY(), 2) + pow(to.getZ() - getZ(), 2));
+        }
+        throw new IllegalStateException("Unknown dimension: " + getDimension());
     }
 }
